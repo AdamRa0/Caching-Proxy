85343400308a6bb5769e7a30858711e26f760104
Adam Rao <adamokoth@protonmail.com>
2024-10-29 21:58:21 +0300

    finish proxy server

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Caching-Proxy
 CLI tool that acts as a proxy server that caches responses from forwarded requests.
+
+
+## Requirements
+- Python 3.12
+- Poetry
+
+## Instructions
+To run this project locally
+- Clone the project
+- cd into clone project and run the following commands
+```bash
+    # Unix systems
+    EXPORT ORIGIN < url to forward traffic to >
+
+    # Windows systems
+    SET ORIGIN < url to forward traffic to >
+
+    poetry shell
+    poetry install
+    pip install -e .
+
+    pproxy run
+```
+
+Visit browser and being using the proxy. For instance, if ORIGIN set to https://reddit.com, visiting localhost:5000/r/asm will take you to the asm subreddit
--- proxy.py
+++ proxy.py
@@ -1,2 +1,37 @@
-def cli() -> None:
-    print("Hello mayne")
+import click
+import requests
+import os
+
+from flask import Flask, Response, request
+from flask.cli import FlaskGroup
+
+def create_app():
+    app = Flask('proxy')
+
+    origin_url = os.getenv("ORIGIN")
+
+    @app.route("/", defaults={"path": ""}, methods=["POST", "GET", "PUT", "PATCH", "UPDATE", "DELETE"])
+    @app.route("/<path:path>", methods=["POST", "GET", "PUT", "PATCH", "UPDATE", "DELETE"])
+    def proxy(path: str):
+        res = requests.request(
+            method          = request.method,
+            url             = request.url.replace(request.host_url, f'{origin_url}/'),
+            headers         = {k:v for k,v in request.headers if k.lower() != 'host'},
+            data            = request.get_data(),
+            cookies         = request.cookies,
+            allow_redirects = False,
+        )
+
+        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+        headers          = [
+            (k,v) for k,v in res.raw.headers.items()
+            if k.lower() not in excluded_headers
+        ]
+
+        response = Response(res.content, res.status_code, headers)
+        return response
+    return app
+
+@click.group(cls=FlaskGroup, create_app=create_app)
+def cli():
+    pass
